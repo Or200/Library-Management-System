5742f077a1ea977e2c89d8c3454df47ad4e004f8
moti jusewitz <motiforstudy123@gmail.com>
2025-11-10 15:33:41 +0200

    the Book Class

--- classes/book.py
+++ classes/book.py
@@ -1,4 +1,12 @@
 
 
 class Book:
-    pass
+
+    def __init__(self, title, author, ISBN, is_available : bool):
+        self.title = title
+        self.author = author
+        self.ISBN = ISBN
+        self.is_available = is_available
+
+    def print_book_info(self):
+        print(f"the title: {self.title}, the author: {self.author}, the ISBN: {self.ISBN}, is available: {self.is_available}")
