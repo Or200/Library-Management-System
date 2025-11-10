7b77cef10ec9cf0b48a6500fef2fa8e145ed0140
moti jusewitz <motiforstudy123@gmail.com>
2025-11-10 15:40:44 +0200

    add type for varaiables

--- classes/book.py
+++ classes/book.py
@@ -2,11 +2,11 @@
 
 class Book:
 
-    def __init__(self, title, author, ISBN, is_available : bool):
+    def __init__(self, title : str, author : str, ISBN : int):
         self.title = title
         self.author = author
         self.ISBN = ISBN
-        self.is_available = is_available
+        self.is_available = True
 
-    def print_book_info(self):
+    def print_book_info(self) -> None:
         print(f"the title: {self.title}, the author: {self.author}, the ISBN: {self.ISBN}, is available: {self.is_available}")
