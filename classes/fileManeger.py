0e65c38ef95368b5eb6d9631b4a522ad1cf0da87
Or200 <avimeda74@gmail.com>
2025-11-10 17:38:53 +0200

    add class FileManeger

--- classes/fileManeger.py
+++ classes/fileManeger.py
@@ -1,4 +1,15 @@
-
+import json
 
 class FileManeger:
-    pass
+
+    def add_to_json(self, list_book: list[object], list_user: list[object]):
+
+        lib = {"books": [book.__dict__ for book in list_book], "users": [user.__dict__ for user in list_user]}
+
+        with open("library.json", "w") as f:
+            json.dump(lib, f, ensure_ascii = False, indent = 4)
+
+    def lode_from_json(self):
+        with open("library.json") as f:
+            lib = json.load(f)
+        return lib
